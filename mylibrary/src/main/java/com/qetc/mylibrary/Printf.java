3ab72a3db2c1205e1af342d4b8bf3077f964bd92
xiehao <18826125375@163.com>
2017-05-18 15:04:09 +0800

    Initial commit 测试MyLibrary

---
+++ mylibrary/src/main/java/com/qetc/mylibrary/Printf.java
@@ -0,0 +1,14 @@
+package com.qetc.mylibrary;
+
+import android.app.Activity;
+import android.widget.Toast;
+
+/**
+ * Created by lenovo on 2017/5/18.
+ */
+
+public class Printf{
+    public String getWord(){
+        return "测试MyLibrary";
+    }
+}
